e647d4fa23319516941fc3e3ce0d709bcab6d569
Bjoern Hansen <b.hansen.ubiqum@gmail.com>
2026-03-27 14:34:31 +0100

    update packages

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 // looks for an env variable called PORT;
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // listen on a port
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
